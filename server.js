6ba80b1d76a2cd5047d159d11a4d2cd820887266
sanand404 <sanand404@gmail.com>
2018-06-23 22:09:18 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/public');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append the log');
+        }
+    });
+    console.log(log);
+    next();
+});
+
+
+app.use( (req, res, next) => {
+    res.render('maintenance.hbs');
+})
+
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+});
+
+app.get("/", (req, res) => {
+    res.render('home.hbs', {
+        pageTitle : 'Home Page',
+        welcomeMessage : 'This is home page message'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('We are live on port 8080');
+});
